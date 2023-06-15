28399999ee0021d95b9fa95df2bc8a1168bfc8b4
Mas-nobb <masimo1009@gmail.com>
2023-06-15 20:21:50 +0900

    バリテーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Devise modules
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Validation
+  validates :nickname, presence: true
+  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } # 全角漢字・ひらがな・カタカナ
+  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } # 全角漢字・ひらがな・カタカナ
+  validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } # 全角カタカナ
+  validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } # 全角カタカナ
+  validates :birthday,           presence: true
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i } # 半角英数字混合
+
+  # Associations
+  has_many :items
+  has_many :purchases
 end
