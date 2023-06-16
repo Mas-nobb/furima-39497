38d3ec004a86ae2409cc14dde062fccea428dc0c
Mas-nobb <masimo1009@gmail.com>
2023-06-16 16:38:20 +0900

    item　コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: "商品が出品されました。"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_day_id, :prefecture_id, :shipping_fee_status_id, :price)
   end
 end
