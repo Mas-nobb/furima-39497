c01313d435cdddeaa5895ed189ec94793d555afc
Mas-nobb <masimo1009@gmail.com>
2023-06-16 16:36:28 +0900

    ストロングパラメーター追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  # バリデーションのカスタムメソッドを定義して、半角数値のみが入力されていることを確認します
+  validate :validate_price_format
+
+  private
+
+  def validate_price_format
+    errors.add(:price, 'は半角数字で入力してください') if price.to_s !~ /\A[0-9]+\z/
+  end
 end
