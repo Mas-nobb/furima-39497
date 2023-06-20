31687cba9c9ba59663b6532ae5de82d3c7b304d6
Mas-nobb <masimo1009@gmail.com>
2023-06-20 20:05:08 +0900

    アソシエーション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @items = Item.all
   end
 
   def new
@@ -9,7 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = current_user.items.build(item_params)
     if @item.save
       redirect_to root_path, notice: "商品が出品されました。"
     else
@@ -22,3 +23,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :shipping_day_id, :prefecture_id, :shipping_fee_status_id, :price, :user_id)
   end
+end
