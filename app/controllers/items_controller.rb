0de502846a6280268443c87d4a21133a47d7fe29
Mas-nobb <masimo1009@gmail.com>
2023-06-20 21:52:37 +0900

    コントローラー、パラメーター修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @items = Item.all
+    @items = Item.order(created_at: :desc)
   end
 
   def new
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = current_user.items.build(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path, notice: '商品が出品されました。'
     else
@@ -22,6 +22,6 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :shipping_day_id, :prefecture_id,
-                                 :shipping_fee_status_id, :price, :user_id)
+                                 :shipping_fee_status_id, :price).merge(user_id: current_user.id)
   end
 end
