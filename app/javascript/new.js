115aa0adb6edebd3169d433b5f24e8a9613ecbbe
Mas-nobb <masimo1009@gmail.com>
2023-06-20 16:48:04 +0900

    JSの編集

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,14 +1,18 @@
-document.getElementById('item-price').addEventListener('input', (e) => {
-  const price = e.target.value;
-  if (!price) {
-      document.getElementById('add-tax-price').innerText = '';
-      document.getElementById('profit').innerText = '';
-      return;
-  }
+function pullDown() {
+  document.getElementById('item-price').addEventListener('input', (e) => {
+    const price = e.target.value;
+    if (!price) {
+        document.getElementById('add-tax-price').innerHTML = '';
+        document.getElementById('profit').innerHTML = '';
+        return;
+    }
 
-  const fee = Math.floor(price * 0.1); // 10%の手数料を計算
-  const profit = price - fee;
+    const fee = Math.floor(price * 0.1); // 10%の手数料を計算
+    const profit = price - fee;
 
-  document.getElementById('add-tax-price').innerText = fee;
-  document.getElementById('profit').innerText = profit;
-});
+    document.getElementById('add-tax-price').innerHTML = fee;
+    document.getElementById('profit').innerHTML = profit;
+  });
+}
+
+document.addEventListener('turbo:load', pullDown); // or 'turbolinks:load' depending on your Rails version
