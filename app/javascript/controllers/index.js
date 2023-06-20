d5283df119e03f7949693764c36f3c3b1c9274e0
Mas-nobb <masimo1009@gmail.com>
2023-06-20 10:31:57 +0900

    手数料自動計算　JSの追加

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,23 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
+document.getElementById('item-price').addEventListener('input', (e) => {
+  const price = e.target.value;
+  if (!price) {
+      document.getElementById('add-tax-price').innerText = '';
+      document.getElementById('profit').innerText = '';
+      return;
+  }
+
+  const fee = Math.floor(price * 0.1); // 10%の手数料を計算
+  const profit = price - fee;
+
+  document.getElementById('add-tax-price').innerText = fee;
+  document.getElementById('profit').innerText = profit;
+});
+
+
+
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
