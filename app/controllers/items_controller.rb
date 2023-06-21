d7febc7d3a190c8b95fdb40d6214fefff370afb5
Mas-nobb <masimo1009@gmail.com>
2023-06-21 11:55:08 +0900

    詳細ページ　コントローラ編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show]
+  # before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order(created_at: :desc)
+  end
+
+  def show
   end
 
   def new
@@ -18,10 +23,37 @@ class ItemsController < ApplicationController
     end
   end
 
+  # def edit
+  # end
+
+  # def update
+  #   if @item.update(item_params)
+  #     redirect_to item_path(@item.id), notice: '商品が更新されました。'
+  #   else
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
+
+  # def destroy
+  #   if @item.destroy
+  #     redirect_to root_path, notice: '商品が削除されました。'
+  #   else
+  #     redirect_to root_path, alert: '商品の削除に失敗しました。'
+  #   end
+  # end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :shipping_day_id, :prefecture_id,
                                  :shipping_fee_status_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  # def move_to_index
+  #   redirect_to root_path unless current_user.id == @item.user_id
+  # end
 end
