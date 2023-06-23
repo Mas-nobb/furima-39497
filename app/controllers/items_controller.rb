34be41e7e1442ddc0fe462e228f9787f580ac5ad
Mas-nobb <masimo1009@gmail.com>
2023-06-23 20:16:52 +0900

    編集機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_item, only: [:show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
   # before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
@@ -23,16 +23,18 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def edit
-  # end
+  def edit
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
+  
 
-  # def update
-  #   if @item.update(item_params)
-  #     redirect_to item_path(@item.id), notice: '商品が更新されました。'
-  #   else
-  #     render :edit, status: :unprocessable_entity
-  #   end
-  # end
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id), notice: '商品が更新されました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   # def destroy
   #   if @item.destroy
